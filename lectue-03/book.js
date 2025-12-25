9780c871191ccf3c2ed51425b676eedcea50a999
MIRE1256 <mill46thawkan@gmail.com>
2025-12-25 15:36:49 +0700

    up

---
+++ lectue-03/book.js
@@ -0,0 +1,14 @@
+// const book = {
+//     title: "1948",
+//     author: "george orwell",
+//     isavilable: true,
+
+//     checkOut: function(){
+//         this.isavilable = false;
+//     }.
+
+
+//     checkIn: function(){
+//         this.isavilable = true;
+//     }
+// };
